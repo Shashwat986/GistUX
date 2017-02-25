4176813225901d4dfe8fa6656fd1c08ee324a28c
Shashwat Chandra <chandra.shashwat@gmail.com>
2017-02-25 13:08:12 +0530

    Moving to webpack build structure. Incomplete

---
+++ src/js/main.js
@@ -0,0 +1,30 @@
+var store = {
+  state: {
+    githubKey: null
+  },
+  setGithubKey (key) {
+    store.state.githubKey = key;
+  },
+  destroySession () {
+    store.state.githubKey = null;
+  }
+}
+
+window.vm = {
+  navbar: new Vue({
+    el: '#navbar',
+    data: {
+      githubKey: null,
+      store: store,
+      state: store.state
+    }
+  }),
+
+  content: new Vue({
+    el: "#main",
+    data: {
+      store: store,
+      state: store.state
+    }
+  })
+}
